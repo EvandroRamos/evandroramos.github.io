db3cf6085e0f56b887d4da0bdf9d562e5a080e5a
EvanArmstrong <evandro_ramos@me.com>
2017-07-23 13:15:15 -0400

    fail

--- js/custom.js
+++ js/custom.js
@@ -106,6 +106,7 @@ jQuery(document).ready(function( $ ) {
       $('html, body').animate({scrollTop : 0},1500, 'easeInOutExpo');
       return false;
   });
-    $(".hero-container").backstretch("img/hero-bg.jpg");
-
+    $("#hero").backstretch("img/hero-bg.jpg");
+    $("#services").backstretch("img/services-bg.jpg");
+    $("#subscribe").backstretch("img/subscribe-bg.jpg");
 });
